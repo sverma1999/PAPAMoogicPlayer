b52e0f09b9731a1f8228953a0a874bd0512aaea3
Shubham <sverma1@unb.ca>
2019-03-22 16:02:35 -0300

    {Shubham}- initial commit

---
+++ app/src/main/java/com/cs2063project/papamoogicplayer/MainActivity.java
@@ -0,0 +1,51 @@
+package com.cs2063project.papamoogicplayer;
+
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListViewForSongs;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myListViewForSongs = (ListView) findViewById(R.id.mySongListView);
+
+        permission();
+    }
+
+    public void permission(){
+
+        Dexter.withActivity(this).withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                        token.continuePermissionRequest();
+
+                    }
+                }).check();
+
+    }
+}
