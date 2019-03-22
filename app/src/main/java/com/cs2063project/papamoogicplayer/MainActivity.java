ef8365fee11dd909988bf118f3591f141c20a44d
Shubham <sverma1@unb.ca>
2019-03-22 17:16:01 -0300

    adding methods for list of songs

--- app/src/main/java/com/cs2063project/papamoogicplayer/MainActivity.java
+++ app/src/main/java/com/cs2063project/papamoogicplayer/MainActivity.java
@@ -2,8 +2,10 @@
 package com.cs2063project.papamoogicplayer;
 
 import android.Manifest;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.karumi.dexter.Dexter;
@@ -13,9 +15,13 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView myListViewForSongs;
+    String[] items;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
 
+                        display();
                     }
 
                     @Override
@@ -49,4 +56,43 @@ public class MainActivity extends AppCompatActivity {
                 }).check();
 
     }
+
+
+    public ArrayList<File> lookForSongs(File file){
+        ArrayList<File> arrayList = new ArrayList<File>();
+
+        File[] files = file.listFiles();
+
+        for(File singleFile: files){
+
+            if(singleFile.isDirectory() && !singleFile.isHidden()){
+                arrayList.addAll(lookForSongs(singleFile));
+            }
+            else{
+                if(singleFile.getName().endsWith(".mp3") ||
+                singleFile.getName().endsWith(".wav")){
+
+                    arrayList.add(singleFile);
+                }
+            }
+
+        }
+
+        return arrayList;
+    }
+
+
+    void display(){
+        final ArrayList<File>  mysongs = lookForSongs(Environment.getExternalStorageDirectory());
+        items = new String[mysongs.size()];
+
+        for(int i =0; i<mysongs.size(); i++ ){
+
+            items[i] = mysongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
+        }
+
+        ArrayAdapter<String> myAdaptor = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
+        myListViewForSongs.setAdapter(myAdaptor);
+    }
+
 }
