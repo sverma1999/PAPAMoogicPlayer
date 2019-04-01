21408ad059721e4522774c8340b626864469e6c0
Shubham <sverma1@unb.ca>
2019-03-31 22:26:54 -0300

    commiting on 31st

---
+++ app/src/main/java/com/cs2063project/papamoogicplayer/fingerPrintAccess.java
@@ -0,0 +1,13 @@
+package com.cs2063project.papamoogicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fingerPrintAccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_print_access);
+    }
+}
